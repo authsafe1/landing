b153b1a2d285624a0dffa7dfe14073a16f924cbd
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2024-12-27 15:57:29 +0530

    code view

---
+++ src/components/ui/CodeViewer.tsx
@@ -0,0 +1,29 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { stackoverflowDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+interface CodeViewerProps {
+  code: string;
+  language?: string;
+}
+
+const CodeViewer: React.FC<CodeViewerProps> = ({
+  code,
+  language = 'javascript',
+}) => {
+  return (
+    <Box component="code" sx={{ maxWidth: '100%', my: 2 }}>
+      <SyntaxHighlighter
+        language={language}
+        style={stackoverflowDark}
+        wrapLines
+        wrapLongLines
+      >
+        {code}
+      </SyntaxHighlighter>
+    </Box>
+  );
+};
+
+export default CodeViewer;
