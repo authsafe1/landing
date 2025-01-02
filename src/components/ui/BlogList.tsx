469860abc298edafe1c42b984b3223c3d2db5bbf
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-01-02 21:26:53 +0530

    search options available

--- src/components/ui/BlogList.tsx
+++ src/components/ui/BlogList.tsx
@@ -1,25 +1,29 @@
 'use client';
 
 import { TypeBlogSkeleton } from '@/utils/blogHelper';
+import { Search } from '@mui/icons-material';
 import {
   Card,
   CardActionArea,
   CardContent,
   Chip,
   Grid2 as Grid,
+  InputAdornment,
+  OutlinedInput,
   Pagination,
   Typography,
 } from '@mui/material';
 import { Entry } from 'contentful';
 import dayjs from 'dayjs';
 import Link from 'next/link';
-import { ChangeEvent, FC, useMemo, useState } from 'react';
+import { ChangeEvent, ChangeEventHandler, FC, useMemo, useState } from 'react';
 
 const BlogList: FC<{
   body: Entry<TypeBlogSkeleton, 'WITHOUT_UNRESOLVABLE_LINKS', string>[];
   total: number;
 }> = ({ body, total }) => {
   const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
   const itemsPerPage = 6;
 
   const handlePaginationChange = (
@@ -29,16 +33,42 @@ const BlogList: FC<{
     setPage(page);
   };
 
-  const filteredBody = useMemo(() => {
+  const handleQueryChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredBlogs = useMemo(() => {
+    return body.filter((blog) =>
+      blog.fields.title.toLowerCase().includes(query.toLowerCase()),
+    );
+  }, [query, body]);
+
+  const paginatedBlogs = useMemo(() => {
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    return body.slice(startIndex, endIndex);
-  }, [page, body]);
+    return filteredBlogs.slice(startIndex, endIndex);
+  }, [filteredBlogs, page]);
+
+  const totalPages = useMemo(() => {
+    return Math.ceil(filteredBlogs.length / itemsPerPage);
+  }, [filteredBlogs]);
 
   return (
     <Grid container spacing={4}>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <OutlinedInput
+          placeholder="Search blogs"
+          onChange={handleQueryChange}
+          endAdornment={
+            <InputAdornment position="end">
+              <Search color="primary" />
+            </InputAdornment>
+          }
+          fullWidth
+        />
+      </Grid>
       <Grid container spacing={2}>
-        {filteredBody.map(({ fields, sys }) => (
+        {paginatedBlogs.map(({ fields, sys }) => (
           <Grid size={{ xs: 12, sm: 6, md: 4 }} key={sys.id}>
             <Card variant="outlined" elevation={2} sx={{ height: '100%' }}>
               <CardActionArea
@@ -66,7 +96,7 @@ const BlogList: FC<{
           <Pagination
             color="primary"
             page={page}
-            count={Math.ceil(total / itemsPerPage)}
+            count={totalPages}
             onChange={handlePaginationChange}
           />
         </Grid>
